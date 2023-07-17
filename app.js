7a13176ae1075c2df9dcbd3bb7e9d6e3937474e3
allaboutwork <afshanalamwork@gmail.com>
2023-07-17 16:40:12 +0530

    added grid to the layout

--- app.js
+++ app.js
@@ -43,5 +43,26 @@ document.addEventListener('DOMContentLoaded', () => {
     // 3 - power-pellet
     // 4 - empty
 
-    
+    const squares = []
+
+    // draw the grid and render it
+    function createBoard(){
+        for (let i =0; i < layout.length; i++){
+            
+            const square = document.createElement('div')
+            grid.appendChild(square)
+            squares.push(square)
+
+            // add layout to the board
+            if(layout[i] === 0){
+                squares[i].classList.add('pac-dot')
+            } else if ( layout[i] === 1){
+                squares[i].classList.add('wall')
+            } else if (layout[i] === 3) {
+                squares[i].classList.add('power-pellet')
+            }
+        }
+    }
+
+    createBoard()
 })
